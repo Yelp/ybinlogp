5927bd0c4d997fd7063ee70efe0d9fbd7523208a
James Brown <jbrown@yelp.com>
2011-09-28 08:35:09 -0700

    improve python test script

--- ybinlogp_test.py
+++ ybinlogp_test.py
@@ -2,10 +2,14 @@ from ybinlogp import YBinlogP
 import sys
 
 bp = YBinlogP(sys.argv[1], always_update=True)
-for i,event in enumerate(bp):
-	print bp.tell()
-	print event
-for i,event in enumerate(bp):
-	print bp.tell()
-	print event
+while True:
+    for i,event in enumerate(bp):
+        if event.event_type == "ROTATE_EVENT":
+            next_file = event.data.file_name
+            bp.clean_up()
+            bp = YBinlogP(next_file, always_update=True)
+            break
+    else:
+        print "Got to end at %r" % (bp.tell(),)
+        break
 bp.clean_up()
