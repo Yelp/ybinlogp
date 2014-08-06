85259e22742f7935e8018f089ecbacae50a0a2f5
Bradley Girardeau <bgirarde@yelp.com>
2014-08-06 10:59:00 -0700

    Add setup.py python build script.

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+from setuptools import setup
+from distutils.command.build import build
+
+class YBinlogPBuild(build):
+    def initialize_options(self):
+        build.initialize_options(self)
+        self.build_base = os.path.join(self.build_base, "python")
+
+    def run(self):
+        subprocess.call(['make', 'build'])
+        build.run(self)
+
+setup(
+    author='Daniel Nephin',
+    author_email='dnephin@yelp.com',
+    cmdclass={'build': YBinlogPBuild},
+    data_files=[('lib', ['build/libybinlogp.so', 'build/libybinlogp.so.1']),
+                ('include', ['src/ybinlogp.h'])],
+    description='Library, program, and python bindings for parsing MySQL binlogs',
+    license='BSD',
+    name='YBinlogP',
+    package_dir={'': 'src'},
+    packages=['ybinlogp'],
+    scripts=['build/ybinlogp'],
+    url='http://github.com/Yelp/ybinlogp',
+    version='0.6'
+)
